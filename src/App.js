b28d2c10418c4d1dae3dac4af51f747fd9245560
MMoniz <user@Matts-MBP.fios-router.home>
2020-07-21 11:52:15 -0400

    added general css styling to inoput field and to submit button

--- src/App.js
+++ src/App.js
@@ -2,25 +2,19 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+class App extends React.Component {
+  render(){
+    return (
+    <div className ="App">
+      <header>
+        <form id="to-do-form">
+          <input type="text" placeholder="Enter Text" />
+          <button type="submit"> Add</button>
+        </form>
       </header>
     </div>
-  );
+    );
+  }
 }
 
 export default App;
