486ae4e3646f3ed989db50b6528ae800003ca8b1
MMoniz <user@Matts-MBP.fios-router.home>
2020-07-24 11:43:53 -0400

    added listitem component to with logic  to add items to the list and some styling

--- src/App.js
+++ src/App.js
@@ -1,17 +1,66 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import ListItems from './ListItems'
 
 class App extends React.Component {
+
+  constructor(props){
+    super(props);
+    this.state={
+      items:[],
+      currentItem:{
+        text:'',
+        key:''
+      }
+    }
+    this.handleInput = this.handleInput.bind(this);
+    this.addItem=this.addItem.bind(this);
+  }
+
+  handleInput(e){
+    this.setState({
+      currentItem:{
+        text: e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+
+  addItem(e){
+    e.preventDefault();
+    const newItem=this.state.currentItem;
+    //checks to see if the newItem is not empty, 
+    //if there is something in state, then it adds it 
+    //to the list to be displayed
+    //it resets the current item state to blank
+    if(newItem.text!==""){
+      const newItems=[...this.state.items,newItem];
+      this.setState({
+        items:newItems,
+        currentItem:{
+          text:'',
+          key:''
+        }
+      })
+    }
+  }
+
   render(){
     return (
     <div className ="App">
       <header>
-        <form id="to-do-form">
-          <input type="text" placeholder="Enter Text" />
+        <form id="to-do-form" onSubmit={this.addItem}>
+          <input 
+          type="text" 
+          placeholder="Enter Text"
+          value={this.state.currentItem.text}
+          onChange={this.handleInput}
+          />
           <button type="submit"> Add</button>
         </form>
       </header>
+      <ListItems items={this.state.items}></ListItems>
     </div>
     );
   }
