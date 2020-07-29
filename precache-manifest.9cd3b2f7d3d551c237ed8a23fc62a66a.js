854fc76d157455747f155adf6f24bba58614958b
MMoniz <user@Matts-MBP.fios-router.home>
2020-07-29 11:32:00 -0400

    Updates

---
+++ precache-manifest.9cd3b2f7d3d551c237ed8a23fc62a66a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "294e7b28739b1c66ca38cfbb0bc6d5a1",
+    "url": "/to-do-list/index.html"
+  },
+  {
+    "revision": "3a42f1f8664c1f6f2b44",
+    "url": "/to-do-list/static/css/main.a30f2beb.chunk.css"
+  },
+  {
+    "revision": "13a5539ad26fb014e262",
+    "url": "/to-do-list/static/js/2.ad0f1f35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/to-do-list/static/js/2.ad0f1f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a42f1f8664c1f6f2b44",
+    "url": "/to-do-list/static/js/main.1ed89b3a.chunk.js"
+  },
+  {
+    "revision": "475cf87243a077512b27",
+    "url": "/to-do-list/static/js/runtime-main.88dcc749.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-list/static/media/logo.5d5d9eef.svg"
+  }
+]);
